f1cec61303c249c4ea5b353d6e3c95e6b9ae854b
康潇潇 <kxx6100@163.com>
2018-11-08 18:42:34 +0800

    123

---
+++ app.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var fs=require("fs");
+var staticPage=fs.readFileSync("./index.html")
+http.createServer(function (request, response) {
+    response.writeHead(200,
+     {'Content-Type': 'text/html'}
+     );
+    response.end(staticPage);
+}).listen(3000);  
